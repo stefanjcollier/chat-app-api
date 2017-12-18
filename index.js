e7410c5db1ce3470dd8ec290afe14e28bb6dbd8c
Joe Cridge <joe.cridge@me.com>
2017-12-18 03:16:00 +0000

    Make initial endpoints a little more idiomatic

--- index.js
+++ index.js
@@ -1,167 +1,84 @@
-const express = require('express');
-
-const app = express();
-
-// Integrate cookies
+const _ = require('lodash')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('[:date] :method :url => :status (:response-time ms)'))
+app.use(cors())
 app.use(cookieParser())
 
+app.locals.chats = require('./data/chats.json')
+app.locals.messages = require('./data/messages.json')
 
-// ======================[ Default Data ]===================================
-app.locals.chats = {
-	'stefan' : { 'joe':[
-			{
-				'sender': 'stefan',
-				'message':'Hiya',
-				'timestamp': '2017-12-01T13:00:00Z',
-			},
-			{
-				'sender': 'joe',
-				'message': 'Hey how are you?',
-				'timestamp': '2017-12-01T14:00:00Z',
-			},
-			]  
-		},
-	'joe'    : { 'stefan': [
-			{
-				'sender': 'joe',
-				'message': 'Hey how are you?',
-				'timestamp': '2017-12-01T14:00:00Z',
-			},
-			{
-				'sender': 'stefan',
-				'message':'Hiya',
-				'timestamp': '2017-12-01T13:00:00Z',
-			},
-		   ], 
-		   'rewaz': [
-		    	{
-				'sender': 'joe',
-				'message':'Hey how are you finding the challenge?',
-				'timestamp': '2017-12-02T08:10:00Z',
-			},
-			{
-				'sender': 'rewaz',
-				'message': 'It is great, react js is rather nice.',
-				'timestamp': '2017-12-02T08:30:00Z',
-			},
-		    	{
-				'sender': 'joe',
-				'message':'I agree, react will take over the world one day.',
-				'timestamp': '2017-12-03T01:10:00Z',
-			},
-			
-		    ]
-		 },
-	'rewaz'  : {  'joe': [
-		    	{
-				'sender': 'joe',
-				'message':'Hey how are you finding the challenge?',
-				'timestamp': '2017-12-02T08:10:00Z',
-			},
-			{
-				'sender': 'rewaz',
-				'message': 'It is great, react js is rather nice.',
-				'timestamp': '2017-12-02T08:30:00Z',
-			},
-		    	{
-				'sender': 'joe',
-				'message':'I agree, react will take over the world one day.',
-				'timestamp': '2017-12-03T01:10:00Z',
-			},
-			
-		    ]
- 		}
-}
-
-/*
- * A nice little standard logger 
- */
-const log = function(req, message) {
-	if ( ! message) {
-		message = "";
-	}
-	console.log(req.method+"   "+req.protocol + '://' + req.get('host') + req.originalUrl+"   "+ message);	
-};
-
-// ======================[ Login Endpoints ]===================================
-/*
- * A method to help you manually check who is logged in
- */
-app.get('/', (req, res) => {
-	log(req);
-  	res.send(`Current user is ${req.cookies.user}`);
-})
-
-/*
- * ‘Log in’ as xyz when visiting /login/xyz
- */
+// Log in as a particular user.
 app.get('/login/:user', (req, res) => {
-	log(req)
-	res.cookie('user', req.params.user)
-	res.send(`Logged in as ${req.params.user}`)
+  res.cookie('user', req.params.user)
+  res.send(`Logged in as ${req.params.user}`)
 })
 
-// ======================[ Chat Endpoints ]===================================
+// Get the list of chats for the logged in user.
+app.get('/chats', (req, res) => {
+  const chatList = []
+  const userChats = app.locals.chats.filter((chat) => chat.users.includes(req.cookies.user))
 
-function compare_on_timestamp(a,b) {
-  if (a.timestamp < b.timestamp)
-    return -1;
-  if (a.timestamp > b.timestamp)
-    return 1;
-  return 0;
-}
+  userChats.forEach((chat) => {
+    const chatMessages = app.locals.messages.filter((message) => message.chat_id === chat.id)
 
+    const lastMessage = _.chain(chatMessages)
+      .sortBy((message) => message.timestamp)
+      .last().value()
 
+    const buddy = _.chain(chat.users)
+      .filter((user) => user !== req.cookies.user)
+      .first().value()
 
-/*
- * Get all the chats for a the logged in user, showing ONLY THE LATEST  message from each chat
- */
-app.get('/chats/', function(req,res) {
-	const me = req.cookies.user
-	const chats = app.locals.chats[me];
-	let shallow_chats = {}
+    chatList.push({
+      buddy,
+      lastMessage: lastMessage.body,
+      timestamp: lastMessage.timestamp
+    })
+  })
 
-	if ( ! chats) {
-		shallow_chats = {}
-	} else {
-		Object.keys(chats).forEach(function(buddy) {
-  			var chat = chats[buddy].sort(compare_on_timestamp);
-			shallow_chats[buddy] = chat[0]
-		});
-
-	}
-	log(req, JSON.stringify(Object.keys(shallow_chats)) )
-	res.send(shallow_chats)
-});
-
-/*
- * Get the ENTIRE conversation between the logged in user and their :buddy
- */
-app.get('/chats/:buddy', function(req,res) {
-	const me = req.cookies.user
-	const buddy = req.params.buddy
-	var chat = app.locals.chats[me][buddy];
-	if ( ! chat) {
-		chat = [] 
-	}
-	log(req, JSON.stringify("Messages:"+chat.length) )
-	res.send(chat)
-});
-
-
-
-// ======================[ Boilerplate  ]===================================
-/*
- * Fallback method to indicate that something has gone wrong
- */
-app.get('*', function(req, res) {
-	log(req, "Undefined endpoint");
-	res.status(404).send('Undefined endpoint');
-});
-
-
-var server = app.listen(8888, function () {
-	console.log('Hello World!');
+  res.json({ chats: chatList })
 })
 
+// Get the list of messages in one of the logged in user's chats.
+app.get('/chats/:buddy/messages', (req, res) => {
+  const buddy = req.params.buddy
+  const user = req.cookies.user
+  const messageList = []
+
+  const chat = _.chain(app.locals.chats)
+    .filter((c) => user !== buddy && (c.users.includes(buddy) && c.users.includes(user)))
+    .first().value()
+
+  if (!chat) {
+    return res.status(404).json({
+      error: `${req.cookies.user} does not have a chat with ${req.params.buddy}`
+    })
+  }
+
+  const messages = app.locals.messages.filter((message) => message.chat_id === chat.id)
+
+  messages.forEach((message) => {
+    messageList.push({
+      body: message.body,
+      sender: message.sender,
+      timestamp: message.timestamp
+    })
+  })
+
+  res.json({ messages: messageList })
+})
+
+// Get the currently logged in user.
+app.get('/', (req, res) => {
+  res.send(`Current user is ${req.cookies.user}`)
+})
+
+app.listen(8888, function () {
+  console.log('Chat server running on http://localhost:8888')
+})
